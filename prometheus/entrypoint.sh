57a909395e0c59ed4c365778955aca59096724d3
Yamil Hernandez <yhernandez@telecontacto.com>
2025-12-31 10:18:40 -0400

    fix: return v

--- prometheus/entrypoint.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-set -e
-envsubst < /etc/prometheus/prom.yml.template > /etc/prometheus/prom.yml
-exec /bin/prometheus --config.file=/etc/prometheus/prom.yml --storage.tsdb.path=/prometheus
